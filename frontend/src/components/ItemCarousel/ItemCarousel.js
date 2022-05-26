5c7a8b30c314a58fa720e816654072bc485d9754
EnzoGRDev <enzo.g.rz.18@gmail.com>
2022-05-26 19:16:33 -0300

    slider de productos

---
+++ frontend/src/components/ItemCarousel/ItemCarousel.js
@@ -0,0 +1,10 @@
+import './ItemCarousel.css'
+
+export default function ItemCarousel ({ title, image }) {
+  return (
+    <figure className='itemCarousel'>
+      <img src={image || 'https://picsum.photos/200/300'} alt={title || 'example'} className='itemCarousel-img' />
+      <h1 className='itemCarousel-title'>{title || 'example'}</h1>
+    </figure>
+  )
+}
--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,5 +1,29 @@
 import './Home.css'
 import banner from '../../assets/img/banner.png'
+import ListProductsSlide from '../../components/ListProductsSlide/ListProductsSlide'
+
+const products = [
+  {
+    id: 1,
+    title: 'Helados',
+    image: 'https://deliciaskitchen.com/wp-content/uploads/2017/07/helado-casero-de-platano-con-frambuesas-y-albahaca.jpg'
+  },
+  {
+    id: 2,
+    title: 'Carnes',
+    image: 'https://www.lavanguardia.com/files/article_main_microformat/files/fp/uploads/2020/09/22/5f69c1aa42a38.r_d.735-595-0.jpeg'
+  },
+  {
+    id: 3,
+    title: 'Vegano',
+    image: 'https://mfacdn.cachefly.net/chooseveg/sites/3/2019/02/arroz-coliflor.jpg'
+  },
+  {
+    id: 4,
+    title: 'Bebidas',
+    image: 'https://i.blogs.es/c65e95/bebida5/450_1000.jpg'
+  }
+]
 
 export default function Home () {
   return (
@@ -9,7 +33,8 @@ export default function Home () {
       <img src={banner} alt='banner' className='home-banner' />
       <div className='home-container-search'>
         <header className='home-container-search__header' />
-        <div />
+        <br />
+        <ListProductsSlide products={products} />
       </div>
     </section>
   )
