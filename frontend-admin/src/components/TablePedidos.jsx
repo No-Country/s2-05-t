9504b0ea44cc623b3e3cda6a2059e85728a988a2
Eliseo Falcon <eliseonop@gmail.com>
2022-06-13 16:43:39 -0500

    update

--- frontend-admin/src/components/TablePedidos.jsx
+++ frontend-admin/src/components/TablePedidos.jsx
@@ -52,7 +52,7 @@ export default function TablePedidos ({
                       Total
                     </th>
                     <th className='px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider'>
-                      Cantidad
+                      N° Productos
                     </th>
                   </tr>
                 </thead>
